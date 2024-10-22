273ef47a7924597590807b63c7700590235a6c1d
francis Jeryckson <francistenang685@gmail.com>
2024-10-22 05:10:35 +0200

    done

---
+++ src/main/java/tech/chillo/position/PositionApplication.java
@@ -0,0 +1,13 @@
+package tech.chillo.position;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PositionApplication.class, args);
+    }
+
+}
